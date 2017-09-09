a49f4d08946c75ca86624cc0b96b36a2d57dd6a4
SKYnv <kvazarv@ya.ru>
2017-09-09 13:52:15 +0700

    rework tasks template with component, add transform for isodate field

--- app/transforms/isodate.js
+++ app/transforms/isodate.js
@@ -2,16 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Transform.extend({
   deserialize(serialized) {
-  	console.log(1, serialized);
   	if (serialized) {
       return moment(serialized).locale('ru').calendar();
     }
-    console.log(0, serialized);
     return serialized;
   },
 
   serialize(deserialized) {
-  	console.log(2, deserialized);
   	if (deserialized) {
       return moment(deserialized).toISOString();
     }
